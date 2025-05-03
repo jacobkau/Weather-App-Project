fa7ade7e95d24d13da30aa97174d8432b726b106
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-05-03 20:58:19 +0300

    Update views.py

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -9,7 +9,7 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
 
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=eb7a9222d551692147f3a293427559e1').read()
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + 'api key').read()
         list_of_data = json.loads(source)
 
         data = {
